acd50bfa5d217f0e5be9473675771fb25552a458
José Matos <62076500+jose-artur90@users.noreply.github.com>
2021-12-07 23:46:03 +0000

    Exercicio populaçao

---
+++ Ex1/main.c
@@ -0,0 +1,42 @@
+/**
+ * @file main.c
+ * José Matos (jose.artur.matos90@gmail.com)
+ * @brief 
+ * version 0.1
+ * 2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int start, end;
+    do{
+        printf("Initial population: \n");
+        scanf("%d",start);        
+    }
+    while(start<9);
+
+    do
+    {
+        printf("End population: \n");
+        scanf("%d",end);
+    } 
+    while (start>end);
+
+    int years = 0;
+    do
+    {
+        start = start + (start/3) - (start/4);
+        years++;
+    } while (start<end);
+    
+    
+    printf("the number is %d\n",years);
+    
+}
+
