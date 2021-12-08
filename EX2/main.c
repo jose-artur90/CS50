bfbbb04875c9c0433ef005a6d4e38a52d2e257cc
José Matos <62076500+jose-artur90@users.noreply.github.com>
2021-12-08 19:02:33 +0000

    new mario

--- EX2/main.c
+++ EX2/main.c
@@ -2,31 +2,32 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main(void)
+int main(int argc, char const *argv[])
 {
+
     int n;
     do
     {
 
     printf("what the height of the pyramid?\n");
-    scanf("%d",n);
-    getchar();
+    scanf("%i",n);
+   
 
-} 
-while (n<1 || n>8);
+    } 
+    while(n<1||n>8);
 
-for (int row = 0; row < n; row++)
-{
-    for (int space = 0; space < n - row - 1; space++)
+    for(int row=0; row<n; row++)
     {
-        printf(" ");
-    }
+        for(int space=0; space<n-row-1; space++)
+        {
+            printf("  ");
+        }
     
-    for (int collum = 0; collum <= row; collum++)
-    {
-        printf("#");
-    }    
-    printf("\n");  
-}
-
+        for(int collum=0; collum<=row; collum++)
+        {
+            printf("#");
+        }    
+        printf("\n");  
+    }
+return 0;
 }
